bf069ad8b03468cf544e8ea220099200864de354
DIMITRI VILERA SARAIVA DE AZEVEDO <dimitri.vsazevedo@senacsp.edu.br>
2022-05-12 15:31:41 -0300

    Terceiro  Projeto  em React 1

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,17 +1,17 @@
-
+import {useState} from "react";
 
 function App() {
 
-let valor1 = Math.round(Math.random() * 15)
-let valor2 = Math.round(Math.random() * 15)
+const [numero, setNumero] = useState(1);
 
 
   return (
     <div>
-      <p>A soma de 50 e 10 é {50+10}</p>
-      <p>O resto da divisão entre 50 e 3 é {50 % 3}</p>
-      <p>A soma entre {valor1} e {valor2} é {valor1+valor2}</p>
-      <p>O resultado da soma acima é uma número {(valor1+valor2) % 2 === 0 ? "Par" : "Impar"}</p>
+      <p>O número atual é {numero}</p>
+<p>
+  <button title="Clique aqui homi" onClick={() => setNumero(numero+1)}>Clique aqui homi</button>
+</p>
+
     </div>
     
   );
